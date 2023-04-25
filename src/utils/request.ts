73c11c877886bd2ac7204f0a9818e212ba557bd6
mingqiangChe <cmq15650272396@163.com>
2023-04-25 20:42:15 +0800

    fix(custom): 修复axios封装警告问题

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,27 +1,40 @@
+import { rejects } from 'assert';
 import axios, { AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 
 const service = axios.create();
 
 // Request interceptors
 service.interceptors.request.use(
-  (config: InternalAxiosRequestConfig) => {
-    // do something
-    return config;
-  },
-  (error: any) => {
-    Promise.reject(error);
-  }
+    (config: InternalAxiosRequestConfig) => {
+        console.log('拦截');
+
+        // do something
+        return config;
+    },
+    (error: any) => {
+        Promise.reject(error);
+    },
 );
 
 // Response interceptors
 service.interceptors.response.use(
-  async (response: AxiosResponse) => {
-    // do something
-  },
-  (error: any) => {
-    // do something
-    return Promise.reject(error);
-  }
+    // async (response: AxiosResponse) => {
+    //   // do something
+    //   console.log('响应', response);
+
+    // },
+    // (error: any) => {
+    //   // do something
+    //   return Promise.reject(error);
+    // }
+    async function (response: AxiosResponse) {
+        console.log('响应');
+
+        return response;
+    },
+    function (error) {
+        return Promise.reject(error);
+    },
 );
 
 export default service;
