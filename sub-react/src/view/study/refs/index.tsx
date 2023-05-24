8ba7325e02c2e518f8a0b7295f13629f5d1d271c
mingqiangChe <cmq15650272396@163.com>
2023-05-24 11:34:34 +0800

    refs

--- sub-react/src/view/study/refs/index.tsx
+++ sub-react/src/view/study/refs/index.tsx
@@ -1,5 +1,6 @@
 import React, {useRef, useState} from 'react';
 import {Input} from 'antd';
+import './index.less';
 
 export default function Refs() {
     // const inputRef = useRef(null);
@@ -10,10 +11,22 @@ export default function Refs() {
         // console.log(inputRef.current.input.value);
         setState(inputRef.current.input.value);
     };
+
+    const img =
+        'https://cdn.hk01.com/di/media/images/dw/20230407/721096484371566592496025.jpeg/Yr5uBeTrCd131oGLy4ZR5mU17uL35sb51cGyv9XBsr8?v=w1920';
     return (
-        <div>
+        <div className='refsblock'>
             <h1>你最喜欢的是{state}</h1>
             <Input ref={inputRef} placeholder='Basic usage' onChange={handleChange} />
+            <img
+                className='avatar'
+                style={{
+                    width: 180,
+                    height: 180
+                }}
+                src={img}
+                alt={'Photo of ' + img}
+            />
         </div>
     );
 }
